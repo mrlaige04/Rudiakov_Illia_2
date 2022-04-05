41b0da987ffdea7f32c51ab478c88d6ac2fd271e
IlliaRudiakov <90439154+mrlaige04@users.noreply.github.com>
2022-04-05 12:00:49 +0300

    lab2_files
    adding all files

---
+++ Lab2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Locality local = new Locality(new int[] { 50, 3, 6, 4, 10 });
+            Console.WriteLine($"Max Height:{local.MaxHeight()}");
+            Console.WriteLine($"Min Height:{local.MinHeight()}");
+            Console.WriteLine("________________________________________");
+            local.HeightKrutyznaDifference();
+            Console.WriteLine("________________________________________");
+            local.Compare();
+
+            
+        }
+    }
+}
